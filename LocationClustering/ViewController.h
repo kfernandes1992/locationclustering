9f74c540f6728b7cc2809a57a9dabf71c9c0cfa4
Kevin Fernandes <User@1x-vl915-72-19-124-59.wireless.umass.edu>
2013-12-09 20:59:44 -0500

    Initial Commit

---
+++ LocationClustering/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocationClustering
+//
+//  Created by Kevin Fernandes on 12/9/13.
+//  Copyright (c) 2013 KevinFernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
